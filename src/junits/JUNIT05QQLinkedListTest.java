9d139fa1efb3c0c89292cb9b8345dd014dc17dcd
RickyCrackers <ricky.tran018@gmail.com>
2026-03-07 17:45:54 -0800

    LinkedList updated

--- src/junits/JUNIT05QQLinkedListTest.java
+++ src/junits/JUNIT05QQLinkedListTest.java
@@ -1,179 +1,179 @@
-//package junits;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertFalse;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//import construction.Point;
-//import utils.LinkedList;
-//
-//
-///**
-// * @author A. Ford
-// * This is a JUnit Test for the utils.LinkedList Class
-// * This class uses the Jupiter JUnit Test Library
-// * current version is Jupiter 5.8.1
-// */
-//public class JUNIT05QQLinkedListTest {
-//
-//    LinkedList<String>   stringList;
-//    LinkedList<Point>    pointList;
-//
-//    @BeforeEach
-//    void testLinkedList() {
-//        stringList = new LinkedList<>();
-//        pointList = new LinkedList<>();
-//        JUNIT02QQListTest.unitTestAddStringElement(stringList);
-//        JUNIT02QQListTest.unitTestAddPointElement(pointList);
-//        assertEquals(stringList.size(), pointList.size());
-//    }
-//
-//
-//    @Test
-//    void testLinkedListListE() {
-//        LinkedList<Integer>list01 = new LinkedList<>();
-//        JUNIT02QQListTest.makeIntegerList(list01);
-//
-//        LinkedList<Integer>list02 = new LinkedList<>(list01);
-//        JUNIT02QQListTest.unitTestAddListElements(list01, list02);
-//    }
-//
-//
-//    @Test
-//    void testAddE() {
-//        JUNIT02QQListTest.unitTestAddElemExpand(stringList, pointList);
-//    }
-//
-//
-//    @Test
-//    void testAddIntE() {
-//        JUNIT02QQListTest.unitTestAddInsertElement(stringList, pointList);
-//    }
-//
-//
-//    @Test
-//    void testAddAllListE(){
-//
-//        LinkedList<Point>list01 = new LinkedList<>();
-//        assertEquals(0, list01.size());
-//
-//        JUNIT02QQListTest.makePointElemStartList(list01);
-//        assertEquals(7, list01.size());
-//
-//        int start = list01.size();
-//
-//        LinkedList<Point> list02 = new LinkedList<>();
-//        assertEquals(0, list02.size());
-//
-//        JUNIT02QQListTest.makePointElemCopyList(list02);
-//        assertEquals(5, list02.size());
-//
-//        assertTrue(list01.addAll(list02));
-//        assertEquals(12, list01.size());
-//
-//        JUNIT02QQListTest.unitTestAddAllList(list01, start);
-//
-//    }
-//
-//
-//    @Test
-//    void testClear() {
-//        JUNIT02QQListTest.unitTestClearList(stringList, pointList);
-//    }
-//
-//
-//    @Test
-//    void testContains() {
-//        JUNIT02QQListTest.unitTestContainsElement(stringList, pointList);
-//    }
-//
-//
-//    @Test
-//    void testEqualsListE() {
-//        LinkedList<Point>list1 = new LinkedList<>();
-//        LinkedList<Point>list2 = new LinkedList<>();
-//        LinkedList<Point>list3 = new LinkedList<>();
-//
-//        JUNIT02QQListTest.makeEqualPointList(list1);
-//        JUNIT02QQListTest.makeEqualPointList(list2);
-//        JUNIT02QQListTest.makeNotEqualPointList(list3);
-//        JUNIT02QQListTest.unitTestEqualListElements(list1, list2, list3);
-//
-//    }
-//
-//
-//    @Test
-//    void testGet() {
-//        JUNIT02QQListTest.unitTestGetElementAtIndex(stringList, pointList);
-//    }
-//
-//
-//    @Test
-//    void testIndexOf() {
-//        JUNIT02QQListTest.unitTestGetIndexOfElement(stringList, pointList);
-//    }
-//
-//
-//    @Test
-//    void testIsEmpty() {
-//
-//        assertFalse(stringList.isEmpty());
-//        assertFalse(pointList.isEmpty());
-//
-//        LinkedList<String>list = new LinkedList<>();
-//
-//        assertTrue(list.isEmpty());
-//
-//        JUNIT02QQListTest.unitTestIsEmptyList(list);
-//
-//    }
-//
-//
-//    @Test
-//    void testRemoveE() {
-//        JUNIT02QQListTest.unitTestRemoveElement(stringList, pointList);
-//    }
-//
-//
-//    @Test
-//    void testRemoveInt() {
-//        JUNIT02QQListTest.unitTestRemoveElementAtIndex(stringList, pointList);
-//    }
-//
-//
-//    @Test
-//    void testRemoveAll() {
-//        LinkedList<Point>list1 = new LinkedList<>();
-//        LinkedList<Point>list2 = new LinkedList<>();
-//
-//        assertEquals(0, list1.size());
-//
-//        JUNIT02QQListTest.unitTestRemoveAllListElements(list1, list2);
-//    }
-//
-//
-//    @Test
-//    void testSet() {
-//        JUNIT02QQListTest.unitTestSetElement(stringList, pointList);
-//    }
-//
-//
-//    @Test
-//    void minCapacity() {
-//        assertEquals(8, stringList.size());
-//        assertEquals(8, pointList.size());
-//    }
-//
-//
-//    @Test
-//    void testToString() {
-//        LinkedList<String>list = new LinkedList<>();
-//        JUNIT02QQListTest.unitTestListOutputToReadableString(list);
-//    }
-//
-//
-//}
-//
+package junits;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import construction.Point;
+import utils.LinkedList;
+
+
+/**
+ * @author A. Ford
+ * This is a JUnit Test for the utils.LinkedList Class
+ * This class uses the Jupiter JUnit Test Library
+ * current version is Jupiter 5.8.1
+ */
+public class JUNIT05QQLinkedListTest {
+
+    LinkedList<String>   stringList;
+    LinkedList<Point>    pointList;
+
+    @BeforeEach
+    void testLinkedList() {
+        stringList = new LinkedList<>();
+        pointList = new LinkedList<>();
+        JUNIT02QQListTest.unitTestAddStringElement(stringList);
+        JUNIT02QQListTest.unitTestAddPointElement(pointList);
+        assertEquals(stringList.size(), pointList.size());
+    }
+
+
+    @Test
+    void testLinkedListListE() {
+        LinkedList<Integer>list01 = new LinkedList<>();
+        JUNIT02QQListTest.makeIntegerList(list01);
+
+        LinkedList<Integer>list02 = new LinkedList<>(list01);
+        JUNIT02QQListTest.unitTestAddListElements(list01, list02);
+    }
+
+
+    @Test
+    void testAddE() {
+        JUNIT02QQListTest.unitTestAddElemExpand(stringList, pointList);
+    }
+
+
+    @Test
+    void testAddIntE() {
+        JUNIT02QQListTest.unitTestAddInsertElement(stringList, pointList);
+    }
+
+
+    @Test
+    void testAddAllListE(){
+
+        LinkedList<Point>list01 = new LinkedList<>();
+        assertEquals(0, list01.size());
+
+        JUNIT02QQListTest.makePointElemStartList(list01);
+        assertEquals(7, list01.size());
+
+        int start = list01.size();
+
+        LinkedList<Point> list02 = new LinkedList<>();
+        assertEquals(0, list02.size());
+
+        JUNIT02QQListTest.makePointElemCopyList(list02);
+        assertEquals(5, list02.size());
+
+        assertTrue(list01.addAll(list02));
+        assertEquals(12, list01.size());
+
+        JUNIT02QQListTest.unitTestAddAllList(list01, start);
+
+    }
+
+
+    @Test
+    void testClear() {
+        JUNIT02QQListTest.unitTestClearList(stringList, pointList);
+    }
+
+
+    @Test
+    void testContains() {
+        JUNIT02QQListTest.unitTestContainsElement(stringList, pointList);
+    }
+
+
+    @Test
+    void testEqualsListE() {
+        LinkedList<Point>list1 = new LinkedList<>();
+        LinkedList<Point>list2 = new LinkedList<>();
+        LinkedList<Point>list3 = new LinkedList<>();
+
+        JUNIT02QQListTest.makeEqualPointList(list1);
+        JUNIT02QQListTest.makeEqualPointList(list2);
+        JUNIT02QQListTest.makeNotEqualPointList(list3);
+        JUNIT02QQListTest.unitTestEqualListElements(list1, list2, list3);
+
+    }
+
+
+    @Test
+    void testGet() {
+        JUNIT02QQListTest.unitTestGetElementAtIndex(stringList, pointList);
+    }
+
+
+    @Test
+    void testIndexOf() {
+        JUNIT02QQListTest.unitTestGetIndexOfElement(stringList, pointList);
+    }
+
+
+    @Test
+    void testIsEmpty() {
+
+        assertFalse(stringList.isEmpty());
+        assertFalse(pointList.isEmpty());
+
+        LinkedList<String>list = new LinkedList<>();
+
+        assertTrue(list.isEmpty());
+
+        JUNIT02QQListTest.unitTestIsEmptyList(list);
+
+    }
+
+
+    @Test
+    void testRemoveE() {
+        JUNIT02QQListTest.unitTestRemoveElement(stringList, pointList);
+    }
+
+
+    @Test
+    void testRemoveInt() {
+        JUNIT02QQListTest.unitTestRemoveElementAtIndex(stringList, pointList);
+    }
+
+
+    @Test
+    void testRemoveAll() {
+        LinkedList<Point>list1 = new LinkedList<>();
+        LinkedList<Point>list2 = new LinkedList<>();
+
+        assertEquals(0, list1.size());
+
+        JUNIT02QQListTest.unitTestRemoveAllListElements(list1, list2);
+    }
+
+
+    @Test
+    void testSet() {
+        JUNIT02QQListTest.unitTestSetElement(stringList, pointList);
+    }
+
+
+    @Test
+    void minCapacity() {
+        assertEquals(8, stringList.size());
+        assertEquals(8, pointList.size());
+    }
+
+
+    @Test
+    void testToString() {
+        LinkedList<String>list = new LinkedList<>();
+        JUNIT02QQListTest.unitTestListOutputToReadableString(list);
+    }
+
+
+}
+
---
+++ src/utils/LinkedList.java
@@ -0,0 +1,263 @@
+package utils;
+
+
+public class LinkedList <E> implements List<E> {
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
+
+    public LinkedList() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+
+    public LinkedList(List<E> list) {
+        first = null;
+        last = null;
+        size = 0;
+        for (int i = 0; i < list.size(); i++) {
+            E item = list.get(i);
+            this.add(item);
+        }
+    }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+        private Node<E> prev;
+
+        public Node(E data) {
+            this(null, data, null);
+        }
+
+        public Node(Node<E> prev, E data) {
+            this(prev, data, null);
+        }
+
+        public Node(Node<E> prev, E data, Node<E> next) {
+            this.prev = prev;
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private void append(E item) {
+        if (first == null) {
+            first = new Node<>(item);
+            last = first;
+        }  else {
+            Node<E> newNode = new Node<>(last, item, null);
+            last.next = newNode;
+            last = newNode;
+        }
+    }
+
+    private Node<E> node(int index) {
+        if (index < size / 2) {
+            Node<E> current = first;
+            for (int i = 0; i < index; i++,  current = current.next) {
+            }
+            return current;
+        } else {
+            Node<E> current = last;
+            for (int i = size - 1 ; i > index; i--,  current = current.prev) {
+            }
+            return current;
+        }
+    }
+
+    private void insertBefore(int index, E item) {
+        Node<E> currentNode = node(index);
+        Node<E> prevNode = currentNode.prev;
+
+        Node<E> newNode = new Node<>(prevNode, item, currentNode);
+
+        currentNode.prev = newNode;
+
+
+        if (prevNode == null) {
+            first = newNode;
+        } else {
+            prevNode.next = newNode;
+        }
+    }
+
+    private void detachNode(Node<E> node) {
+        if (node == null) {
+            return;
+        }
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            first = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            last = node.prev;
+        }
+
+        node.data = null;
+        node.next = null;
+        node.prev = null;
+    }
+
+    private E detach(int index) {
+        Node<E> targetNode = node(index);
+        E oldNoteData =  targetNode.data;
+        detachNode(targetNode);
+        return oldNoteData;
+    }
+
+    private void detachNodeAfter(Node<E> node) {
+        if (node != null && node.next  != null) {
+            detachNode(node.next);
+        }
+    }
+
+    private void detachNodeBefore(Node<E> node) {
+        if (node != null && node.prev  != null) {
+            detachNode(node.prev);
+        }
+    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        return node(index).data;
+    }
+
+    public E set(int index, E item) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        Node<E> targetNode = node(index);
+        E oldNoteData = targetNode.data;
+        targetNode.data = item;
+        return oldNoteData;
+    }
+
+    public void add(int index, E item) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+
+        if  (index == size) {
+            append(item);
+        } else {
+            insertBefore(index, item);
+        }
+        size++;
+    }
+
+    public boolean add(E item) {
+        append(item);
+        size++;
+        return true;
+    }
+
+    public boolean addAll(List<E> list) {
+        for (int i = 0; i < list.size(); i++) {
+            add(list.get(i));
+        }
+        return true;
+    }
+
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+        E oldNoteData = detach(index);
+        size--;
+        return oldNoteData;
+    }
+
+    public boolean remove(E item) {
+        int index = indexOf(item);
+        if (index == -1) {
+            return false;
+        }
+        remove(index);
+        return true;
+    }
+
+    public boolean removeAll(List<E> list) {
+        boolean change = false;
+        for (int i = 0; i < list.size(); i++) {
+            E target = list.get(i);
+            for (; remove(target); ) {
+                change = true;
+            }
+        }
+        return change;
+    }
+
+    public void clear() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    public int indexOf(E item) {
+        int index = 0;
+        for (Node<E> node = first; node != null; node = node.next, index++) {
+            if (node.data.equals(item)) {
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contains(E item) {
+        return indexOf(item) != -1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof List)) {
+            return false;
+        }
+        List<E> other = (List<E>) obj;
+
+        if (this.size != other.size()) {
+            return false;
+        }
+
+        Node<E> current = first;
+
+        for (int i = 0; i < size; i++) {
+            if (!current.data.equals(other.get(i))) {
+                return false;
+            }
+            current = current.next;
+        }
+        return true;
+    }
+
+    public String toString() {
+        if (isEmpty()) {
+            return "[]";
+        } else {
+            StringBuilder result = new StringBuilder("[" + first.data);
+            for (Node<E> node = first.next; node != null; node = node.next) {
+                result.append(", ").append(node.data);
+            }
+            return result.append("]").toString();
+        }
+    }
+
+
+}
